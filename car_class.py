93d9d3c530c7066110d49b93da21df1ecceb1834
John Wells <molekewl2017@gmail.com>
2019-08-14 03:33:43 -0700

    added classes

--- car_class.py
+++ car_class.py
@@ -38,10 +38,35 @@ my_new_car.update_odometer(100)
 my_new_car.read_odometer()
 
 class ElectricCar(Car):
-    """Represents aspects of a ar, pecific to electric vehicles."""
+    """Represents aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
+        # self.battery_size = 70
+        self.battery = Battery()
         
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+        
+    def get_range(self):
+        """Print a statement about the rang this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+            
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
