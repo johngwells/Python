051d7bf059c4b1d9e1c796a85e9aa97e5e0ed93d
John Wells <molekewl2017@gmail.com>
2019-08-14 01:53:25 -0700

    exercises

--- greeter.py
+++ greeter.py
@@ -15,3 +15,12 @@ def greet_user(username):
     print("Hello, " + username.title() + "!")
     
 greet_user('jesse')
+
+def greet_users(names):
+    """Print a simple reeting to each user in the list."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
