f8eb7177684a448b20a100d34fc1cc4dbf4c13bb
John Wells <molekewl2017@gmail.com>
2019-08-13 17:08:34 -0700

    python exercise

--- formatted_name.py
+++ formatted_name.py
@@ -9,3 +9,14 @@ def get_formatted_name(first_name, last_name, middle_name=''):
 
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
+
+# return a dictionary
+def build_person(first_name, last_name, age=''):
+    """Return a dictionary of information about a person"""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+artist = build_person('alex', 'gray', 50)
+print(artist)
